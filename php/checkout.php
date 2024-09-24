7183670f2dd4476576f2c53c5df1edcdfdfe7342
JTS-Games <1066300@hr.nl>
2024-09-24 11:31:37 +0200

    styling bling

--- php/checkout.php
+++ php/checkout.php
@@ -1,5 +1,6 @@
 <?php
 require_once 'database.php';
+
 ?>
 
 <!DOCTYPE html>
@@ -8,8 +9,35 @@ require_once 'database.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Checkout</title>
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
+    <header>
+        <div class="headerLeft">
+            <a class="headerLeftText" href="../index.php">CyberNoir</a>
+        </div>
 
+        <div class="headerRight">
+            <div class="headerRightPart">
+                <a class="blueText" class="headerRightText" href="../html/signup.html">Sign Up</a>
+            </div>
+            <div class="headerRightPart">
+                <a class="headerRightText" href="../html/login.html">Log In</a>
+            </div>
+        </div>
+    </header>
+    <main>
+        <section class="mainCheckout">
+            <div class="checkoutInformation">
+                <p>Meep left</p>
+            </div>
+            <div class="checkoutForm">
+                <p>Meep right</p>
+            </div>
+        </section>
+    </main>
+    <footer>
+        <p class="footerP">© 2074 CyberNoir</p>
+    </footer>
 </body>
 </html>
